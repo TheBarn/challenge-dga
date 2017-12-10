8677cf6204ee1e326e3bf643c9093eae10264247
Paul <33380838+pdespres@users.noreply.github.com>
2017-12-10 17:21:57 +0100

    Update test_gensim_query.py

--- test_gensim_query.py
+++ test_gensim_query.py
@@ -1,2 +1,13 @@
 from gensim import corpora, models, similarities
 dictionary = corpora.Dictionary.load('/tmp/simul.dict')
+corpus = corpora.MmCorpus('/tmp/simul.mm')
+lsi = models.load('/tmp/model.lsi')
+
+search = ? # saisie de recherche
+vec_bow = dictionary.doc2bow(doc.lower().split())
+vec_lsi = lsi[vec_bow]
+
+index = similarities.MatrixSimilarity(lsi[corpus])
+sims = index[vec_lsi]
+sims = sorted(enumerate(sims), key=lambda item: -item[1])
+print(sims)
