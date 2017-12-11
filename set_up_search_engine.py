eb0777fcb7a1f1a70bc2b96b5b464941b8803e51
TheBarn <thebarn@protonmail.com>
2017-12-11 11:07:16 +0100

    some revision on Ambroise code

--- set_up_search_engine.py
+++ set_up_search_engine.py
@@ -1,15 +1,25 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Dec  7 15:04:39 2017
-
 @author: Ambroise Prevel
 """
 
-
 from os import listdir
 from os.path import join, isdir, isfile
+from whoosh.fields import Schema, TEXT, ID
+from whoosh.analysis import StandardAnalyzer
+from nltk.corpus import stopwords
+import os.path
+from whoosh.index import create_in
+from whoosh.index import open_dir
+from whoosh.qparser import QueryParser
+from whoosh.qparser import MultifieldParser, FuzzyTermPlugin
+import nltk
 
 def get_files_from_dir(path_to_dir, r):
+    """
+    recursive function to retrieve the paths of the files
+    """
     
     if not isdir(path_to_dir):
         return
@@ -20,42 +30,25 @@ def get_files_from_dir(path_to_dir, r):
         elif isfile(path_to_output):
             r.append(path_to_output)
     return(r)
-    
 
-from whoosh.fields import Schema, TEXT, ID
-from whoosh.analysis import StandardAnalyzer
-from nltk.corpus import stopwords
-
-
-# Création du schéma de l'index
-set_of_stopwords = set(stopwords.words('english')).union(set(stopwords.words('french')))
-schema = Schema(path=ID(stored=True), 
-                title=TEXT(stored=True), 
-                content=TEXT(analyzer=StandardAnalyzer(stoplist=set_of_stopwords)))
-
-
-# Création de l'index
-
-import os.path
-from whoosh.index import create_in
-from whoosh.index import open_dir
-from whoosh.qparser import QueryParser
-
-if not os.path.exists("index"):
-    os.mkdir("index")
-    ix = create_in("index", schema)
-
-ix = open_dir("index")
-
-# Si pas encore fait, ajout des docs dans l'index
-
-if ix.doc_count() != 3376:
+def create_index():
+    """
+    Create a whoosh index for all files
+    """
+    nltk.download('stopwords')
+    # Création du schéma de l'index
+    set_of_stopwords = set(stopwords.words('english')).union(set(stopwords.words('french')))
+    schema = Schema(path=ID(stored=True), title=TEXT(stored=True), content=TEXT(analyzer=StandardAnalyzer(stoplist=set_of_stopwords)))
+    # Création de l'index
+    if not os.path.exists("index"):
+        os.mkdir("index")
+        ix = create_in("index", schema)
+    ix = open_dir("index")
     writer = ix.writer()
-    
     result = []
-    files_to_import = get_files_from_dir("C:\\Users\\Ambroise Prevel\\Dropbox\\HEC\\Cours\\Digital\\DGA\\formated_data",
-                                         r = result)
+    files_to_import = get_files_from_dir("/Users/thebarn/challenge-dga/formated_data", r = result)
     for f in files_to_import:
+    #on ferme le seacher
         with open(f, 'r', encoding='utf-8', errors='surrogateescape') as handle:
             cont = handle.read()
         titl = f.split('\\')[-1]
@@ -63,10 +56,6 @@ if ix.doc_count() != 3376:
         print(titl)    
     print('COMMITING')
     writer.commit()
-
-
-from whoosh.qparser import MultifieldParser, FuzzyTermPlugin
-
 
 def search_in_index(search_kw, index):
     '''
@@ -82,15 +71,17 @@ def search_in_index(search_kw, index):
         correction pour chaque mot entré par l'utilisateur. A voir comment on 
         mélange les suggestions des différents mots pour fournir des suggestions 
         complètes
-
     '''
-    parser = MultifieldParser(["content", "title"], index.schema) #on utilise un MultifieldParser pour rechercher à la fois dans le titre et dans le contenu
-    parser.add_plugin(FuzzyTermPlugin()) #on rajoute un plugin de FuzzyMatching pour pouvoir chercher au delà des mots exacts
+    #on utilise un MultifieldParser pour rechercher à la fois dans le titre et dans le contenu
+    parser = MultifieldParser(["content", "title"], index.schema)
+    #on rajoute un plugin de FuzzyMatching pour pouvoir chercher au delà des mots exacts
+    parser.add_plugin(FuzzyTermPlugin()) 
     searcher = index.searcher()
-    to_parse = ' '.join([i+'~1' for i in search_kw.split(' ')]) #on transforme la requête utilisateur pour la mettre en format compréhensible par le plugin de FuzzyMatching
+    #on transforme la requête utilisateur pour la mettre en format compréhensible par le plugin de FuzzyMatching
+    to_parse = ' '.join([i+'~1' for i in search_kw.split(' ')]) 
     myquery = parser.parse(to_parse)
-    r = searcher.search(myquery)
     #on récupère les résultats pour pouvoir fermer le searcher  par la suite   
+    r = searcher.search(myquery)
     results = []
     for res in r:
         results.append({'title': res['title'], 'path': res['path']})
@@ -99,24 +90,23 @@ def search_in_index(search_kw, index):
     suggestions = {}    
     for kw in search_kw.split(' '):
         suggestions[kw] = corrector.suggest(kw)
-    
-    searcher.close() #on ferme le seacher
-    
+    #on ferme le seacher
+    searcher.close() 
     return {'results': results, 'suggestions': suggestions}    
 
-#démo de cette fonction:
-r = search_in_index('pomitical tentions', ix)
-resultats = r['results']
-print("MEILLEUR RESULTAT:")
-print(resultats[0])
-print("\nSUGGESTIONS DE MOTS CLEFS")
-suggestions = r['suggestions']
-print(suggestions)
 
+def test_search_function():
+    ix = open_dir("index")
+    #démo de cette fonction:
+    r = search_in_index('pomitical tentions', ix)
+    print(r)
+    resultats = r['results']
+    print("MEILLEUR RESULTAT:")
+    print(resultats[0])
+    print("\nSUGGESTIONS DE MOTS CLEFS")
+    suggestions = r['suggestions']
+    print(suggestions)
 
-
-    
-
-    
-    
-
+if __name__ == "__main__":
+    #create_index()
+    test_search_function()
