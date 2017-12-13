946b36b73794a32214e70a667b8e3e01f427c0ed
Paul <33380838+pdespres@users.noreply.github.com>
2017-12-13 12:33:20 +0100

    Update test_gensim_query.py

--- test_gensim_query.py
+++ test_gensim_query.py
@@ -1,9 +1,11 @@
 from gensim import corpora, models, similarities
+import numpy
 
 def search(search_kw):
     dictionary = corpora.Dictionary.load('simul.dict')
     corpus = corpora.MmCorpus('simul.mm')
     lsi = models.LsiModel.load('model.lsi')
+    docNames = numpy.load('index_dict.npy').item()
 
     vec_bow = dictionary.doc2bow(search_kw.lower().split())
     vec_lsi = lsi[vec_bow]
@@ -11,7 +13,10 @@ def search(search_kw):
     index = similarities.MatrixSimilarity(lsi[corpus])
     sims = index[vec_lsi]
     result = sorted(enumerate(sims), key=lambda item: -item[1])
-    return(result)
+    result_files[]
+    for r in result
+        result_files.append(docnames[r])
+    return(result_files)
 
 def main():
     return_lst = search('deepthought')
