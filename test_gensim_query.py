b056525e0b52876f9f7dadfee73bdbca8a0eb22c
Paul <33380838+pdespres@users.noreply.github.com>
2017-12-14 16:17:30 +0100

    Update test_gensim_query.py

--- test_gensim_query.py
+++ test_gensim_query.py
@@ -6,20 +6,20 @@ def search(search_kw):
     corpus = corpora.MmCorpus('simul.mm')
     lsi = models.LsiModel.load('model.lsi')
     docNames = numpy.load('index_dict.npy').item()
-
+ 
     vec_bow = dictionary.doc2bow(search_kw.lower().split())
     vec_lsi = lsi[vec_bow]
-
     index = similarities.MatrixSimilarity(lsi[corpus])
     sims = index[vec_lsi]
     result = sorted(enumerate(sims), key=lambda item: -item[1])
     result_files = []
-    for r in result[0]:
-        result_files.append(docNames[r])
+    for r in result:
+        result_files.append(docNames[r[0]])
     return(result_files)
+ #   return(result)
 
 def main():
-    return_lst = search('farcy')
+    return_lst = search('political military')
     return(return_lst)
 
 if __name__ == "__main__":
